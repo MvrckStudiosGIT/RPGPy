1ab327cbcf5d7030940aa7fe6987d39c374a5786
MvrckStudiosGIT <151485072+MvrckStudiosGIT@users.noreply.github.com>
2023-11-23 11:19:01 +1000

    Add files via upload

---
+++ RPGPy_Mvrck_Studios/RPGPy.py
@@ -0,0 +1,23 @@
+import keyboard
+
+print("Thanks for Downloading RPGPy!")
+print("RPGPy - A Python Game Engine by Harrison Horton")
+
+textinput = ""
+playerImage = ""
+npcText = ""
+dialouge = ""
+npcImage = ""
+
+def text(textInput):
+    print(textInput)
+
+def player(playerImage):
+    print(playerImage)
+
+class npc:
+    def npcBody(npcImage):
+        print(npcImage)
+
+    def npcDialouge(dialouge):
+        print(dialouge)
